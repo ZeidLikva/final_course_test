edcaa31a6272d3624370e3fbcbcdfedd2a9d9d30
Inga-Viktoriia Kuksova <na.yada1990@gmail.com>
2020-10-31 00:43:56 +0300

    Finish

--- test_main_page.py
+++ test_main_page.py
@@ -9,9 +9,11 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+def go_to_login_page(self):
+    link = self.browser.find_element_by_css_selector("#login_link")
+    link.click()
+    alert = self.browser.switch_to.alert
+    alert.accept()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
