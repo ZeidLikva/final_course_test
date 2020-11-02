ed8f59696db9555f60afd1405582246639f136c4
Inga-Viktoriia Kuksova <na.yada1990@gmail.com>
2020-11-02 19:16:22 +0300

    add login_page to base_page and make test for it

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 from .pages.product_page import ProductPage
-import time
+from .pages.login_page import LoginPage
 import pytest
 
+@pytest.mark.skip
 @pytest.mark.parametrize('n', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, n):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{n}"
@@ -13,3 +14,18 @@ def test_guest_can_add_product_to_basket(browser, n):
     page.is_right_product()
     page.should_be_basket_price()
     page.is_right_price()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
